eeedb705d913b26f8b805e295da7b6b17ef20a63
jencekade <gaephuoc@gmail.com>
2017-11-06 00:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ function scuberGreetingForFeet(feet) {
 }
 
 function ternaryCheckCity(city) {
-  city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  (city == 'NYC' ? 'Ok, sounds good.' : 'No go.');
 
 }
 
-function switchOnCharmFromTip() {
-
-
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
